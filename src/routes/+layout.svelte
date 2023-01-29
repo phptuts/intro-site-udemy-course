9f1ad73abd86964ee8dfb914d3d4db90923e8a89
phptuts <glaserpower@gmail.com>
2023-01-28 19:04:22 -0800

    7-prefetch-link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <nav>
-	<li><a href="/">Home</a></li>
-	<li><a href="contact">Contact</a></li>
-	<li><a href="about">About</a></li>
+	<li><a data-sveltekit-preload-data href="/">Home</a></li>
+	<li><a data-sveltekit-preload-data href="contact">Contact</a></li>
+	<li><a data-sveltekit-preload-data href="about">About</a></li>
 </nav>
 <h1>{data.name}</h1>
 <main>
